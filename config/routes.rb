038b152edf55c45ba10de62359f686f7047ce2e9
LayMui <mui_ume@hotmail.com>
2025-02-22 21:33:20 +0800

    Initial FE touch up on devise template

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
