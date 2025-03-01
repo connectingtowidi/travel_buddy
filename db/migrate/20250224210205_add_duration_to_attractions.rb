10fb250cf6b9272424dbaacaa6ef67fc152f1fe9
Asher Devang <asherd.2045@gmail.com>
2025-03-01 16:21:11 +0800

    scrape TA api

--- db/migrate/20250224210205_add_duration_to_attractions.rb
+++
@@ -1,5 +0,0 @@
-class AddDurationToAttractions < ActiveRecord::Migration[7.1]
-  def change
-    add_column :attractions, :duration, :integer
-  end
-end
--- db/migrate/20250225055917_drop_travels.rb
+++
@@ -1,5 +0,0 @@
-class DropTravels < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :travels
-  end
-end
