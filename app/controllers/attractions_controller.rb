021184d167323aa1739546520d326bb336ebb03c
amandasulistyo <amandaz.sulistyo@gmail.com>
2025-03-01 15:59:56 +0800

    needed to add attraction show page in order to start implementing hotwire. also added the show method in controller

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,9 +1,12 @@
 # app/controllers/attractions_controller.rb
 
 class AttractionsController < ApplicationController
-    def index
-      attractions = TripadvisorApi.fetch_singapore_attractions
-      render json: attractions
-    end
+  def index
+    attractions = TripadvisorApi.fetch_singapore_attractions
+    render json: attractions
+  end
+
+  def show
+  end
+
 end
-  
