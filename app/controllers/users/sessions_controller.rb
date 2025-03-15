9ddf0eb7117488c95c09ae3da7d2365d6f85090c
LayMui <mui_ume@hotmail.com>
2025-03-15 09:47:19 +0800

    Remove redundant sessions controller

--- app/controllers/users/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class Users::SessionsController < Devise::SessionsController
-    def new
-        self.resource = resource_class.new(sign_in_params)
-        clean_up_passwords(resource)
-        yield resource if block_given?
-        render partial: "devise/shared/auth_form" 
-      end
-  end
